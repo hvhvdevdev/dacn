b0a599aa792d8303398c4d6ee043875e56adbcb4
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2023-09-25 13:09:20 +0700

    fix activated generator

--- pages/generators/index.vue
+++ pages/generators/index.vue
@@ -36,20 +36,23 @@ export default {
             'Jekyll is a static site generator written in Ruby by Tom Preston-Werner. It is distributed under the open source MIT license',
           active: true,
           link: '/generators/jekyll',
+          kind: 'jekyll.yml',
         },
         {
           title: 'Zola',
           description:
             'A fast static site generator in a single binary with everything built-in',
           active: false,
-          link: '/generators/zola'
+          link: '/generators/zola',
+          kind: 'zola.yml',
         },
         {
           title: 'Hugo',
           description:
             'Hugo is a static site generator written in Go. Steve Francia originally created Hugo as an open source project in 2013',
           active: false,
-          link: '/generators/hugo'
+          link: '/generators/hugo',
+          kind: 'hugo.yml',
         },
       ],
     }
@@ -59,6 +62,11 @@ export default {
       return [...this.generators].sort((_a, b) => b.active ?? 1)
     },
   },
+  mounted() {
+    for (const g of this.generators) {
+      g.active = this.$store.getters['generator/getGenerator'] === g.kind
+    }
+  }
 }
 </script>
 
